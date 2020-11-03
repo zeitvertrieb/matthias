ba3cc5e77c47f67dc5e448d48c4b030cd4a9c227
Matthias Grieder <matthias.grieder@ebcont.com>
2020-11-03 19:57:06 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,7 +30,7 @@ module.exports = {
     description: `Design and User Experience`,
     author: `@zeitvertrieb`,
   },
-  pathPrefix: `/mg`,
+  pathPrefix: `/matthias`,
   plugins: [
     `gatsby-plugin-dark-mode`,
     `gatsby-transformer-remark`,
